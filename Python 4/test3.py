5f03bc3f1ad5ca610d033d799f7d06daf8601659
Mazenabosedo <abosedomazen@gmail.com>
2022-09-19 17:03:27 +0300

    This is Python project

---
+++ Python 4/test3.py
@@ -0,0 +1,7 @@
+#Python 25:
+#Python reading files:
+workers_file = open("workers.txt","r")
+print(workers_file.readable())#readable يرجعلك boolian value اذا كان ممكن نقرأها او لا
+print(workers_file.read())#read يقرأ الملف ويطبعه
+print(workers_file.readlines()[1])#readlines يقرأ الملف ويطبعه على طريقة الليست وممكن تحدد السطر اللي بدك تقرأه
+workers_file.close()#close يجب اغلاق الملف بعد فتحه
