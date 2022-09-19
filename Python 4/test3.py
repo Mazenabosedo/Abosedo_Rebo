06a6eae1de88186e7a65d19792f5b7beab0bc9d5
Mazen23123 <113595840+Mazen23123@users.noreply.github.com>
2022-09-19 17:12:45 +0300

    Python project
    This is the project that you want me to do

--- Python 4/test3.py
+++ Python 4/test3.py
@@ -5,3 +5,4 @@ print(workers_file.readable())#readable يرجعلك boolian value اذا كان
 print(workers_file.read())#read يقرأ الملف ويطبعه
 print(workers_file.readlines()[1])#readlines يقرأ الملف ويطبعه على طريقة الليست وممكن تحدد السطر اللي بدك تقرأه
 workers_file.close()#close يجب اغلاق الملف بعد فتحه
+# Mazenabosedo
